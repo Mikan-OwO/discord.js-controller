7abe9709b7c5d5d50f6090e07b4f290aca245c83
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-09-02 01:05:53 +0900

    Update Client.js

--- src/client/Client.js
+++ src/client/Client.js
@@ -1,2 +1,15 @@
-class Client extends EventEmitter {
+class client extends EventEmitter {
+ constructor(client, options) {
+  super();
+  this.client = client;
+  this.options = options;
+  this.ready = true; 
+ }
+ aaa(message){
+  //code
+  this.emit("message", message);
+ }
+ get bbb(){
+  return this.ready;
+ }
 }
